57e4a1c7e8441c58550643a61c73418d7c032a2e
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-05 15:27:27 -0400

    <feat> Refactor database connection to utils file
    Moved logic for establishing a database connection to utils.go file.
    main.go is thus cleaner and more focused on the application logic.

---
+++ backend/utils.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DriverName string
+	UserName   string
+	Password   string
+	Host       string
+	Port       string
+	DbName     string
+	SSLMode    string
+}
+
+func getPostgresConfig() Config {
+	const envFileName string = "postgres.env"
+	err := godotenv.Load(envFileName)
+
+	if err != nil {
+		log.Fatalf("Error loading secret.env file: %v", err)
+	}
+
+	config := Config{
+		DriverName: os.Getenv("POSTGRES_DRIVER"),
+		UserName:   os.Getenv("POSTGRES_USER"),
+		Password:   os.Getenv("POSTGRES_PASSWORD"),
+		Host:       os.Getenv("POSTGRES_HOST"),
+		Port:       os.Getenv("POSTGRES_PORT"),
+		DbName:     os.Getenv("POSTGRES_DB"),
+		SSLMode:    os.Getenv("POSTGRES_SSL_MODE"),
+	}
+
+	return config
+}
+
+func connectToPostgres() *sql.DB {
+	config := getPostgresConfig()
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		config.UserName, config.Password, config.Host, config.Port, config.DbName, config.SSLMode)
+
+	db, err := sql.Open(config.DriverName, connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
