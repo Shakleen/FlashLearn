aafd0132cf5603383ee6f3ae26ee9a38eb1ab603
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-05 12:51:16 -0400

    <feat> Move postgres config to .env file
    Commit @724efb used hardcoded connection string to connect to postgres
    database. That is not good practice and thus it was moved to a .env
    file.
    
    The connection String is built using information read from the .env file
    which contains the following keys:
        1. POSTGRES_HOST
        2. POSTGRES_PORT
        3. POSTGRES_USER
        4. POSTGRES_PASSWORD
        5. POSTGRES_DB
        6. POSTGRES_SSL_MODE
        7. POSTGRES_DRIVER
    
    This ensures that later on the database can be changed without having to
    change the code.

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	const driverName string = "postgres"
-	const connStr string = "postgres://postgres:flashlearn@localhost:5432/fl-db?sslmode=disable"
+type Config struct {
+	DriverName string
+	UserName   string
+	Password   string
+	Host       string
+	Port       string
+	DbName     string
+	SSLMode    string
+}
 
-	db, err := sql.Open(driverName, connStr)
+func main() {
+	config := getPostgresConfig()
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		config.UserName, config.Password, config.Host, config.Port, config.DbName, config.SSLMode)
+
+	db, err := sql.Open(config.DriverName, connStr)
 
 	if err != nil {
 		log.Fatal(err)
@@ -22,4 +37,25 @@ func main() {
 	}
 
 	defer db.Close()
+}
+
+func getPostgresConfig() Config {
+	const envFileName string = "postgres.env"
+	err := godotenv.Load(envFileName)
+
+	if err != nil {
+		log.Fatalf("Error loading secret.env file: %v", err)
+	}
+
+	// Get environment variables
+	config := Config{
+		DriverName: os.Getenv("POSTGRES_DRIVER"),
+		UserName:   os.Getenv("POSTGRES_USER"),
+		Password:   os.Getenv("POSTGRES_PASSWORD"),
+		Host:       os.Getenv("POSTGRES_HOST"),
+		Port:       os.Getenv("POSTGRES_PORT"),
+		DbName:     os.Getenv("POSTGRES_DB"),
+		SSLMode:    os.Getenv("POSTGRES_SSL_MODE"),
+	}
+	return config
 }
