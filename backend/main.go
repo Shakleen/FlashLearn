724efb1d10518be94c4e546457edad0964b2d6e6
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-05 12:26:10 -0400

    <feat> Connect to postgres database
    Created a temporary database on a docker postgres image. Used postgres
    package in go to connect to this database with proper error handling.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	const driverName string = "postgres"
+	const connStr string = "postgres://postgres:flashlearn@localhost:5432/fl-db?sslmode=disable"
+
+	db, err := sql.Open(driverName, connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
