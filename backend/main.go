7882b703a7ea411466d339cd35b1082e9c0b8334
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-06 15:07:44 -0400

    <refactor> Move APIServer to internal/api
    This is to reflect recent refactoring initiatives to improve code organization
    and maintainability. The APIServer struct and its associated methods
    have been moved to a new package located at backend/internal/api.

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flash-learn/internal/api"
 	"fmt"
 
 	_ "github.com/lib/pq"
@@ -9,13 +10,14 @@ import (
 func main() {
 	db := connectToPostgres()
 
-	server := NewAPIServer("localhost:8080", db)
+	server := api.NewAPIServer("localhost:8080", db)
 	err := server.Run()
 
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		return
 	}
+
 	fmt.Println("Server started on localhost:8080")
 
 	defer db.Close()
