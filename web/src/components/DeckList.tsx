b764f2a98f1f09783d27c17d33f60682482fabab
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-08 11:34:46 -0400

    <feat> Hover and click deck item
    Visual feedback to user on hovering a certain deck item. Also added click
    listener on mouse click.

--- web/src/components/DeckList.tsx
+++ web/src/components/DeckList.tsx
@@ -9,11 +9,20 @@ interface DeckListProps {
 }
 
 function DeckList(props: DeckListProps) {
+  const handleItemClick = (item: any) => {
+    console.log("Item clicked:", item);
+  };
+
   return (
     <>
       <ul className="list-group">
         {props.items.map((item) => (
-          <li className="list-group-item d-flex justify-content-between align-items-start">
+          <li
+            key={item.id}
+            className="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            onClick={() => handleItemClick(item)}
+            style={{ cursor: "pointer" }}
+          >
             <div className="ms-2 me-auto">
               <div className="fw-bold">{item.name}</div>
               {item.description}
