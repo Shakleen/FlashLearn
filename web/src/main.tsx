091af2fec12591635e53c45dc912efd4eae03a79
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-08 17:15:33 -0400

    <lib> Setup react router for navigation purposes
    React router package allows for ease of navigation. It connects each address
    of the website to a specific page.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
