7b71035897bab3d14bd65bf49566c6bc6aabf4af
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-04-08 21:13:43 -0400

    <feat> Deck form submission
    Implemented the deck form submission. Handles both the creation and
    updating of a deck through two separate endpoints.

--- web/src/pages/DeckFormPage.tsx
+++ web/src/pages/DeckFormPage.tsx
@@ -1,13 +1,85 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import NavBar from "../components/NavBar";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function DeckFormPage() {
+  const { id } = useParams();
   const navigate = useNavigate();
+  const [nameMaxLength, setNameMaxLength] = useState(0);
+  const [descriptionMaxLength, setDescriptionMaxLength] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseNameMaxLength = await fetch(
+        "http://localhost:8080/deck/nameMaxLength"
+      );
+      const resultNameMaxLength = await responseNameMaxLength.json();
+      setNameMaxLength(resultNameMaxLength["maxLength"]);
+
+      const responseDescriptionMaxLength = await fetch(
+        "http://localhost:8080/deck/descriptionMaxLength"
+      );
+      const resultDescriptionMaxLength =
+        await responseDescriptionMaxLength.json();
+      setDescriptionMaxLength(resultDescriptionMaxLength["maxLength"]);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const payload = Object.fromEntries(formData);
+    const deckName = payload["deckName"];
+    const deckDescription = payload["deckDescription"];
+
+    var deckID = parseInt(id || "-1");
+    console.log(deckID);
+
+    if (deckID == -1) {
+      // Insert POST /deck
+      const response = await fetch("http://localhost:8080/deck", {
+        method: "POST",
+        body: JSON.stringify({
+          name: deckName,
+          description: deckDescription,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Http error! Status: ${response.status}`);
+      }
+
+      const rawData = await response.json();
+      deckID = rawData["id"];
+    } else {
+      // Update POST /deck/{id}
+      const response = await fetch(`http://localhost:8080/deck/${deckID}`, {
+        method: "POST",
+        body: JSON.stringify({
+          name: deckName,
+          description: deckDescription,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Http error! Status: ${response.status}`);
+      }
+
+      const rawData = await response.json();
+      deckID = rawData["id"];
+    }
+
+    navigate(-1);
+  };
 
   return (
     <>
       <NavBar />
-      <form className="container m-3">
+      <form className="container m-3" onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="deckName" className="form-label">
             Deck Name
@@ -15,12 +87,14 @@ function DeckFormPage() {
           <input
             type="text"
             className="form-control"
-            id="deckName"
+            name="deckName"
             aria-describedby="deckNameHelp"
             placeholder="Computer Science"
+            maxLength={nameMaxLength}
+            required
           />
           <div id="deckNameHelp" className="form-text">
-            This is the name of the deck.
+            This is the name of the deck. <b>Max length: {nameMaxLength}</b>
           </div>
         </div>
         <div className="mb-3">
@@ -30,11 +104,13 @@ function DeckFormPage() {
           <input
             type="text"
             className="form-control"
-            id="deckDescription"
+            name="deckDescription"
             placeholder="This a deck containing all the topics in computer science"
+            maxLength={descriptionMaxLength}
           />
           <div id="deckDescriptionHelp" className="form-text">
-            This is the description of the deck.
+            This is the description of the deck.{" "}
+            <b>Max length: {descriptionMaxLength}</b>
           </div>
         </div>
         <button type="submit" className="btn btn-primary">
